0496d5cf83d48fb9e362e49249e3204bf3e08b93
Kate <katya_tsarkov@mail.ru>
2023-02-01 21:24:48 +0300

    view.py

--- view.py
+++ view.py
@@ -1,1 +1,16 @@
 import controller
+from tkinter import *
+
+def set_new_contact():
+    elements = controller.get_elements()
+    name = elements['name'].get()
+    surname = elements['surname'].get()
+    number = elements['number'].get()
+    contact = f'{name} {surname} : {number}'
+    controller.add_to_listbox(contact)
+    
+def print_contacts_list(contacts_list):
+    for contact in contacts_list:
+        #print(contact)
+        #if contact != ' ':
+        controller.add_to_listbox(contact)
